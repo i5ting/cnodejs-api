79b5c9e47b41997c704cbe3a617aa6deeaedc0da
i5ting <i5ting@126.com>
2016-09-27 00:17:48 +0800

    lazycommmit, please ignore

--- create.js
+++ create.js
@@ -3,7 +3,7 @@
 import test from 'ava';
 
 test.cb('POST /api/v1/topics', function (t) {
-  require('..').create('多岁的发生的范德萨', '我是用来测试的我是用来测试的我是用来测试的我是用来测试的我是用来测试的我是用来测试的我是用来测试的 胜多负少的')
+  require('..').create('cnodejs api测试信息请忽略', '我是用来测试的我是用来测试的我是用来测试的我是用来测试的我是用来测试的我是用来测试的我是用来测试的 请忽略')
     .then(function(response){
       t.true(response["success"])
       t.end()
