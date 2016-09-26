eee7205c7f2cfa7cdf8eb2b35403ab3801c00744
i5ting <i5ting@126.com>
2016-09-26 19:57:42 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var debug = require('debug')('weide')
+const debug = require('debug')('cnodejs')
 
-var argv = process.argv;
-argv.shift();
-
-var file_path = __dirname;
-var current_path = process.cwd();
-
-var home = '/Applications/微信web开发者工具.app/'
-
-
-if(undefined !== process.env.CNODE_TOKEN) {
-  home = process.env.CNODE_TOKEN + '/'
+if (!process.env.CNODE_TOKEN) {
+  debug('请提供授权token，然后配置export.CNODE_TOKEN=xxxx')
 }
-
