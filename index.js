78a5d1c61c733741839da9ee7f376e4f807f2530
i5ting <i5ting@126.com>
2016-09-26 20:01:03 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const debug = require('debug')('cnodejs')
 
 if (!process.env.CNODE_TOKEN) {
   debug('请提供授权token，然后配置export.CNODE_TOKEN=xxxx')
+  console.log('请提供授权token，然后配置export.CNODE_TOKEN=xxxx')
 }
