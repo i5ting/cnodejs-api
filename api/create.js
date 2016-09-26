1dc427e398a271c9a3bfa930af342f6f686d7770
i5ting <i5ting@126.com>
2016-09-26 19:13:12 +0800

    lazycommmit, please ignore

---
+++ api/create.js
@@ -0,0 +1,38 @@
+var request = require('request')
+//
+// 接收 post 参数
+//
+// - accesstoken String 用户的 accessToken
+// - title String 标题
+// - tab String 目前有 ask share job
+// - content String 主体内容
+// 
+// 返回值示例
+//
+// {success: true, topic_id: '5433d5e4e737cbe96dcef312'}
+
+module.exports = function create(title, content, category) {
+  var _category = 'share'
+  if (category) {
+    _category = category;
+  }
+  var f = {
+    accesstoken: process.env.CNODE_TOKEN,
+    title: title,
+    content: content,
+    tab: _category
+  }
+  
+  console.log(process.env.CNODE_TOKEN)
+  console.log(f)
+  
+  request.post({
+    url:'https://cnodejs.org/api/v1/topics', 
+    form: f
+  }, function(err,httpResponse,httpResponse){
+     /* ... */ 
+    console.log(err)
+    console.log(httpResponse)
+    console.log(httpResponse)
+  })
+}
